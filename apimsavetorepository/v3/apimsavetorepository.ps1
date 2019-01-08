153a5c7cbb70154f94509502644d5ab51aa52630
Justin Marshall <j.marshall@revers.io>
2019-01-08 14:31:52 +0100

    Add a new task for save API to repository

---
+++ apimsavetorepository/v3/apimsavetorepository.ps1
@@ -0,0 +1,66 @@
+[CmdletBinding()]
+param()
+Trace-VstsEnteringInvocation $MyInvocation
+try {
+<#  
+Warning: this code is provided as-is with no warranty of any kind.
+This task saves the state of your API MAnagement portal to the repository. 
+Prerequisite to using this task: the API Gateway requires connectivity to the backend, so make sure these are either public, either part of a
+shared VNET
+#>	
+	#getting inputs
+	    $arm=Get-VstsInput -Name ConnectedServiceNameARM
+		$Endpoint = Get-VstsEndpoint -Name $arm -Require	
+		$portal=Get-VstsInput -Name ApiPortalName
+		$rg=Get-VstsInput -Name ResourceGroupName 
+
+	#preparing endpoints	
+		$client=$Endpoint.Auth.Parameters.ServicePrincipalId
+		$secret=[System.Web.HttpUtility]::UrlEncode($Endpoint.Auth.Parameters.ServicePrincipalKey)
+		$tenant=$Endpoint.Auth.Parameters.TenantId		
+		$body="resource=https%3A%2F%2Fmanagement.azure.com%2F"+
+        "&client_id=$($client)"+
+        "&grant_type=client_credentials"+
+        "&client_secret=$($secret)"
+	    try
+		{
+			#getting ARM token
+			$resp=Invoke-WebRequest -UseBasicParsing -Uri "https://login.windows.net/$($tenant)/oauth2/token" `
+				-Method POST `
+				-Body $body| ConvertFrom-Json    
+		
+		}
+			catch [System.Net.WebException] 
+			{
+				$er=$_.ErrorDetails.Message.ToString()|ConvertFrom-Json
+				write-host $er.error.details
+				throw
+			}
+		
+		$headers = @{
+			Authorization = "Bearer $($resp.access_token)"        
+		}
+		$json = '{
+					"branch" : "master"
+				}'
+		
+		write-host $json
+		$baseurl="$($Endpoint.Url)subscriptions/$($Endpoint.Data.SubscriptionId)/resourceGroups/$($rg)/providers/Microsoft.ApiManagement/service/$($portal)"
+		$targeturl="$($baseurl)/tenant/configuration/save?api-version=2017-03-01"	
+		Write-Host "Saving to repository $($targeturl)"
+		try
+		{
+			Invoke-WebRequest -UseBasicParsing -Uri $targeturl -Headers $headers -Body $json -Method Put -ContentType "application/json"
+		}
+		catch [System.Net.WebException] 
+		{
+			$er=$_.ErrorDetails.Message.ToString()|ConvertFrom-Json
+			Write-Host $er.error.details
+			throw
+		}
+		
+		Write-Host $rep
+
+} finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
