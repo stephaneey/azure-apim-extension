13b1957f82448728cb670ca9ef966a875f3e7bb1
Serge Aradj <saradj@powell-software.com>
2022-03-24 17:20:34 +0100

    First draft

--- apimoperationpolicies/v5/apimoperationpolicies.ps1
+++ apimoperationpolicies/v5/apimoperationpolicies.ps1
@@ -11,7 +11,8 @@ This task creates an APIM product.
 	$portal=Get-VstsInput -Name ApiPortalName
 	$rg=Get-VstsInput -Name ResourceGroupName
 	$api=Get-VstsInput -Name ApiName
-	$operation=Get-VstsInput -Name OperationName
+	$operations=@(Get-VstsInput -Name OperationName)
+	$UseApiCreatedByPreviousTask=Get-VstsInput -Name UseApiCreatedByPreviousTask
 	$SelectedTemplate=Get-VstsInput -Name TemplateSelector
 	$CurrentRevision=Get-VstsInput -Name CurrentRevision
 	
@@ -114,16 +115,55 @@ This task creates an APIM product.
 			else {
 				$rev=$revisions.count
 			}
-			Write-Host "Revision to patch is $($rev)"
-			$policyapiurl=	"$($baseurl)/apis/$($api);rev=$($rev)/operations/$($operation)/policies/policy?api-version=2017-03-01"
-			$JsonPolicies = "{
-				`"properties`": {					
-				`"policyContent`":`""+$PolicyContent+"`"
+
+			if($UseApiCreatedByPreviousTask -eq $true)
+			{
+				if ($null -eq $env:NewUpdatedApi)
+				{
+					throw "There was no operations created by a previous task"
 				}
-			}"
-			Write-Host "Linking policy to API USING $($policyapiurl)"
-			Write-Host $JsonPolicies
-			Invoke-WebRequest -UseBasicParsing -Uri $policyapiurl -Headers $headers -Method Put -Body $JsonPolicies -ContentType "application/json"
+				
+				try
+				{
+					$api = $env:NewUpdatedApi
+
+					Write-Host "Listing operations from $($api)"
+
+					$apioperationsurl = "$($baseurl)/apis/$($api);rev=$($rev)/operations/$($operation)?api-version=2017-03-01"
+
+					$operationsData = Invoke-RestMethod -UseBasicParsing -Uri $apioperationsurl -Headers $headers -Method Get
+
+					$operations = $operationsData.value | Select-Object -ExpandProperty name
+	
+					if ($operations.Length -le 0)
+					{
+						$operations = $env:NewUpdatedApi
+					}
+	
+					Write-Host "Number of operations created by a previous task(s): $($operations.Length)"
+				}
+				catch [System.Net.WebException] 
+				{
+					$er=$_.ErrorDetails.Message.ToString()|ConvertFrom-Json
+					Write-Host $er.error.details
+					throw
+				}
+			}
+			
+			Write-Host "Revision to patch is $($rev)"
+			
+			foreach ($operation in $operations)
+			{
+				$policyapiurl=	"$($baseurl)/apis/$($api);rev=$($rev)/operations/$($operation)/policies/policy?api-version=2017-03-01"
+				$JsonPolicies = "{
+					`"properties`": {					
+						`"policyContent`":`""+$PolicyContent+"`"
+					}
+				}"
+				Write-Host "Linking policy to API USING $($policyapiurl)"
+				Write-Host $JsonPolicies
+				Invoke-WebRequest -UseBasicParsing -Uri $policyapiurl -Headers $headers -Method Put -Body $JsonPolicies -ContentType "application/json"
+			}
 		}
 		catch [System.Net.WebException] 
 		{
